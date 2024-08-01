eebca33998aa8d8d5986d909e3e84e4a90b4941b
mnazal <mohammednazalm@gmail.com>
2024-08-01 17:35:51 +0530

    minor update

--- ffantasy_app/lib/main.dart
+++ ffantasy_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ffantasy_app/bloc/bottomnavbar_bloc/bottom_nav_bar_bloc.dart';
 import 'package:ffantasy_app/screens/home_page.dart';
+import 'package:ffantasy_app/screens/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
